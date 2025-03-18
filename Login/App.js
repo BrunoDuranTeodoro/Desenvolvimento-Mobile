fd8dff316dad871e12d73357f7a088c735d86670
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-17 21:56:27 -0300

    LOGIN

---
+++ Login/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import App2 from './Componentes/AppLogin';
+
+
+export default function App() {
+  return (
+      <App2></App2>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
